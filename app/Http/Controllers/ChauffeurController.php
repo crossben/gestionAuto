70ee77e3d430e972661fd1bf33f0f71eca0d21d4
= <=>
2024-02-20 16:06:32 +0000

    Commit

---
+++ app/Http/Controllers/ChauffeurController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chauffeur;
+use Illuminate\Http\Request;
+
+class ChauffeurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chauffeur $chauffeur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chauffeur $chauffeur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chauffeur $chauffeur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chauffeur $chauffeur)
+    {
+        //
+    }
+}

b166cc5809ed62bad36034839aae5272d742e7dd
ben <gueco167@gmail.com>
2024-03-06 05:03:14 +0000

    ajout des route de vehicule

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VehiculeController;
 use App\Http\Controllers\Auth\ChauffeurController;
 
 /*
@@ -24,24 +25,31 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    //User routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     Route::get('/create', [ChauffeurController::class, 'create']);
     Route::get('/index', [ChauffeurController::class, 'index'])->name('add_chauffeur');
     Route::post('/index', [ChauffeurController::class, 'store'])->name('store_chauffeur');
 
-
+    //Chauffeurs routes
     Route::get('/supprimer_chauffeur/{id}', [ChauffeurController::class, 'supprimer_chauffeur']);
     Route::get('/modifier_chauffeur/{id}', [ChauffeurController::class, 'modifier_chauffeur']);
     Route::post('/modifier/traitement', [ChauffeurController::class, 'modifier_chauffeur_traitement']);
     Route::get('/chauffeurs', [ChauffeurController::class, 'index']);
     Route::get('/ajouter', [ChauffeurController::class, 'ajouter_chauffeur'])->name("ajouter_chauffeur");
     Route::post('/ajouter/traitement', [ChauffeurController::class, 'ajouter_chauffeur_traitement'])->name('ajouter_chauffeur_traitement');
+
+    //Vehicule routes
+    Route::get('/create', [VehiculeController::class, 'create']);
+    Route::get('/index', [VehiculeController::class, 'index'])->name('add_vehicule');
+    Route::post('/index', [VehiculeController::class, 'index'])->name('store_vehicule');
+    Route::get('/edit', [VehiculeController::class, 'edit'])->name('edit_vehicule');
+    Route::get('/update', [VehiculeController::class, 'update'])->name('update');
+    Route::get('/destroy', [VehiculeController::class, 'destroy'])->name('destroy_vehicule');
 });
 
-// route pour les bails de chauffeur 
 
 
 

46a1d7a1f4dd86d8ff2c813ef535a74b77377fc6
ben <gueco167@gmail.com>
2024-03-10 22:35:59 +0000

    ajout de la route edit et locations sur le dossier locations

--- resources/views/auth/locations/edit_location.blade.php
+++ resources/views/auth/locations/edit_location.blade.php
@@ -2,6 +2,53 @@
 
 @section('content')
     <div class="container mt-5 pt-5">
-        
+        <form action="{{ route('location_traitement') }}">
+            @csrf
+            <h1>Ajouter une location</h1>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="lieu_depart" class="form-label">Lieu depart</label>
+                        <input type="text" name="lieu_depart" id="lieu_depart" class="form-control">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="lieu_arrivee" class="form-label">Lieu arrivee</label>
+                        <input type="text" name="lieu_arrivee" id="lieu_arrivee" class="form-control">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="date" class="form-label">Date</label>
+                        <input type="text" name="date" id="date" class="form-control">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="heure_debut" class="form-label">Heure debut</label>
+                        <input type="text" name="heure_debut" id="heure_debut" class="form-control">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="heure_fin" class="form-label">Heure fin</label>
+                        <input type="text" name="heure_fin" id="heure_fin" class="form-control">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">valider</button>
+            </div>
+        </form>
     </div>
 @endsection

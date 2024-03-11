232ae70782a514f2bc07aff41f7b2722df804a24
= <=>
2024-03-11 13:46:32 +0000

    changement html

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Carpooling</a>
+      <a class="navbar-brand" href="#">YOBALEMA</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -20,10 +20,10 @@
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">My Rides</a>
+            <a class="nav-link" href="#">Mes Locations</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">New Ride</a>
+            <a class="nav-link" href="#">Nouvelle Location</a>
           </li>
         </ul>
       </div>
@@ -35,9 +35,8 @@
         <thead>
           <tr>
             <th>Date</th>
-            <th>Start</th>
-            <th>End</th>
-            <th>Seats</th>
+            <th>Debut</th>
+            <th>Fin</th>
             <th>Status</th>
           </tr>
         </thead>
